1d5f58e50d9e9e653a92f7491e7fdf40c5e446f4
Yugandhara Lad <yugandharalad9@gmail.com>
2017-10-12 12:05:53 -0400

    Added StartOver Button

--- MakeYourOwnAdventure/ViewController.swift
+++ MakeYourOwnAdventure/ViewController.swift
@@ -12,14 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Start Over", style: .plain, target: self, action: #selector(startOver))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func startOver()  {
+        if let navigationController = navigationController {
+            navigationController.popToRootViewController(animated: true)
+        }
     }
-
-
-}
+    
+    deinit {
+        print("VC deallocated")
+    }
+ 
+    }
 
